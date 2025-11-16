7d7e2fbc78aaf3005234afefaa68f1725f4517f6
Horus Turboss <81076765+docteur-turboss@users.noreply.github.com>
2025-11-16 14:58:07 +0100

    :wrench:(common): 🚚 Migrate the structure file

--- Trader/src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from "./api/app";
-// import { logger } from "./services/logger";
-
-const port = process.env.EXPRESS_PORT || 3000;
-
-app.listen(port, () => {
-//   logger.info(`Backend running on port ${port}`)
-});
