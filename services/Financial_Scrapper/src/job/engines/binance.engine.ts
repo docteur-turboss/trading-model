e7003489f5c6c7e250f08315bcb089bbc9efebce
Horus Turboss <81076765+docteur-turboss@users.noreply.github.com>
2025-11-25 09:59:04 +0100

    :sparkles:(common): Add engines logic to the jobs (cron & worker)

---
+++ services/Financial_Scrapper/src/job/engines/binance.engine.ts
@@ -0,0 +1,22 @@
+import { ClandlestrickData } from "../../clients/binance/binance.client";
+import { CandleDataFetcher } from "./engine.types";
+import { sleep } from "../../utils/sleep";
+
+type fetchType = Parameters<typeof ClandlestrickData>
+
+export class BinanceEngine implements CandleDataFetcher {
+  async fetchCandles({ symbol, interval, startTime, limit } : {symbol: string; interval: fetchType[2]; startTime: number; limit: number}) {
+    return await ClandlestrickData(symbol, limit, interval, startTime);
+  }
+
+  async handleError(err: any) {
+    if (err?.response?.status === 418) {
+      console.warn("[binance] Ban detected → sleep 2 min");
+      await sleep(120000);
+    }
+    if (err?.response?.status === 429) {
+      console.warn("[binance] Rate-limit → sleep 30s");
+      await sleep(30000);
+    }
+  }
+}
