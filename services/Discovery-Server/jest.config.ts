76b3c933c6e0bbb1322f288622f6916cc973c25e
Horus Turboss <81076765+docteur-turboss@users.noreply.github.com>
2025-12-25 19:40:28 +0100

    📦️ Update mock loading on jest

--- services/Discovery-Server/jest.config.ts
+++ services/Discovery-Server/jest.config.ts
@@ -139,7 +139,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setup/jest.setup.ts"]
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -198,4 +198,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default config;  
