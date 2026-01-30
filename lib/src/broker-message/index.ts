cd98fe8d099496eae463326e5d64465f8d41737d
Horus Turboss <81076765+docteur-turboss@users.noreply.github.com>
2026-01-30 20:32:16 +0100

    :construction:(common): End of the day commit

--- lib/src/broker-message/index.ts
+++ lib/src/broker-message/index.ts
@@ -1,11 +1,20 @@
 import { HttpClient } from "config/httpClient";
+import addressManagerClient from "adress-manager/index";
+import { ServiceInstanceName } from "config/services.types";
+import { MessageManagerClient } from "./client/messageManagerClient";
 
 export default class {
     private httpClient: HttpClient;
+    private MessageManagerClient: MessageManagerClient;
+    private topics: string[]|null = null;
+
     constructor(
+        private instanceId: string,
         private RootCACertPath: string,
         private CertificatPath: string,
-        private KeyCertificatPath: string
+        private KeyCertificatPath: string,
+        private addressManagerClient: addressManagerClient,
+        private serviceName: keyof typeof ServiceInstanceName,
     ) {
         this.httpClient = new HttpClient({
             ca: RootCACertPath,
@@ -13,6 +22,20 @@ export default class {
             key: KeyCertificatPath
         });
 
-        
+        this.MessageManagerClient = new MessageManagerClient(this.httpClient, {
+            callbackPath: "message",
+            instanceId,
+            serviceName
+        }, addressManagerClient)
+    }
+
+    async listenTopics (topics: string[]): Promise<void> {
+        await this.MessageManagerClient.SubscribeToTopics(topics);
+        this.topics = topics
+    }
+
+    async stopMessageManager (): Promise<void> {
+        await this.MessageManagerClient.UnSubscribeToTopic(this.topics??[])
+        this.topics = null;
     }
 }
