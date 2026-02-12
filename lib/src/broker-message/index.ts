c4252e6a53f62c75586d1556f40cb0c6378d9285
Horus Turboss <81076765+docteur-turboss@users.noreply.github.com>
2026-02-12 19:25:11 +0100

    💥 Modify the instance definition (broker-message)
    Update the constructor definition, migrate the listenTopics to intents (with strict type), add on listener (like discordjs) and listenExpress function;

--- lib/src/broker-message/index.ts
+++ lib/src/broker-message/index.ts
@@ -1,21 +1,38 @@
+import { Application } from "express";
 import { HttpClient } from "config/httpClient";
 import addressManagerClient from "adress-manager/index";
 import { ServiceInstanceName } from "config/services.types";
+import { CreateCallbackRoute } from "./http/messages.routes";
 import { MessageManagerClient } from "./client/messageManagerClient";
+import { EventEnumMap, EventMessagesArgs } from "config/event.types";
+import { EventManager, Listener } from "./client/eventManagerClient";
 
 export default class {
-    private httpClient: HttpClient;
     private MessageManagerClient: MessageManagerClient;
-    private topics: string[]|null = null;
+    private topics: EventEnumMap[]|null = null;
+    private event: (() => void)[] = [];
+    private callbackPath: string = "message";
+    private httpClient: HttpClient;
 
-    constructor(
-        private instanceId: string,
-        private RootCACertPath: string,
-        private CertificatPath: string,
-        private KeyCertificatPath: string,
-        private addressManagerClient: addressManagerClient,
-        private serviceName: keyof typeof ServiceInstanceName,
-    ) {
+    constructor({
+        addressManagerClient,
+        KeyCertificatPath,
+        RootCACertPath,
+        CertificatPath,
+        callbackPath,
+        instanceId,
+        serviceName,
+    }: { 
+        instanceId: string,
+        callbackPath?: string,
+        RootCACertPath: string,
+        CertificatPath: string,
+        KeyCertificatPath: string,
+        addressManagerClient: addressManagerClient,
+        serviceName: keyof typeof ServiceInstanceName,
+    }) {
+        this.callbackPath = callbackPath?callbackPath:this.callbackPath;
+
         this.httpClient = new HttpClient({
             ca: RootCACertPath,
             cert: CertificatPath,
@@ -23,19 +40,28 @@ export default class {
         });
 
         this.MessageManagerClient = new MessageManagerClient(this.httpClient, {
-            callbackPath: "message",
+            callbackPath: this.callbackPath,
             instanceId,
             serviceName
-        }, addressManagerClient)
+        }, addressManagerClient);
     }
 
-    async listenTopics (topics: string[]): Promise<void> {
+    async intents (topics: EventEnumMap[]): Promise<void> {
         await this.MessageManagerClient.SubscribeToTopics(topics);
-        this.topics = topics
+        this.topics = topics;
     }
 
     async stopMessageManager (): Promise<void> {
-        await this.MessageManagerClient.UnSubscribeToTopic(this.topics??[])
+        await this.MessageManagerClient.UnSubscribeToTopic(this.topics??[]);
+        this.event.forEach((killFunction) => killFunction());
         this.topics = null;
+    }
+
+    on(event: EventEnumMap, listener: Listener<EventMessagesArgs<EventEnumMap>>) {
+        this.event.push(EventManager.on(event, listener));
+    }
+
+    async listenExpress (app: Application) {
+        app.use(CreateCallbackRoute(this.callbackPath));
     }
 }
