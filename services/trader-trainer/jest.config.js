9b858b3b6ac74c2341917d4f4c79daef4a3798f7
docteur-turboss <81076765+docteur-turboss@users.noreply.github.com>
2026-06-06 15:42:17 +0200

    :white_check_mark:(trader-trainer): set minimum test coverage to 80%

--- services/trader-trainer/jest.config.js
+++ services/trader-trainer/jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
