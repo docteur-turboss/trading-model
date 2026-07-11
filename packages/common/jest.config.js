5a90b13dd586a8b95e23c0581e5fbe846df3f95e
docteur-turboss <81076765+docteur-turboss@users.noreply.github.com>
2026-07-12 01:43:18 +0200

    :recycle:(common): add moduleNameMapper, set maxWorkers, fix coverage threshold indentation

--- packages/common/jest.config.js
+++ packages/common/jest.config.js
@@ -4,14 +4,18 @@ module.exports = {
   testEnvironment: 'node',
   rootDir: '.',
   testMatch: ['**/?(*.)+(spec).ts'],
+  moduleNameMapper: {
+    '^@trading-model/common/(.*)$': '<rootDir>/src/$1',
+  },
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
+  maxWorkers: 10,
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
 };
