3a1d63b2a24023af3da1390e44fa376d7644d9c3
docteur-turboss <81076765+docteur-turboss@users.noreply.github.com>
2026-09-14 01:50:29 +0200

    :wrench:(common): update config

--- packages/common/jest.config.js
+++ packages/common/jest.config.js
@@ -12,12 +12,8 @@ module.exports = {
     ],
   },
   testMatch: ['**/?(*.)+(spec).ts'],
-  moduleNameMapper: {
+moduleNameMapper: {
     '^@trading-model/http/(.*)$': '<rootDir>/../http/src/$1',
-    '^@trading-model/jobs/(.*)$': '<rootDir>/../jobs/src/$1',
-    '^@trading-model/crypto/(.*)$': '<rootDir>/../crypto/src/$1',
-    '^@trading-model/validation/(.*)$': '<rootDir>/../validation/src/$1',
-    '^@trading-model/server-utils/(.*)$': '<rootDir>/../server-utils/src/$1',
     '^@trading-model/common/(.*)$': '<rootDir>/src/$1',
   },
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
